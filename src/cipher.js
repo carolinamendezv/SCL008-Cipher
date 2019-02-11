1f73de9c17b56f61fca60a1ffd1ee6328f439a89
carolinamendezv <nina.mendezv@gmail.com>
2019-02-11 15:55:59 -0400

    funcion encode y decode lista, uniendo cipher con DOM

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,36 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+window.cipher ={
+  encode:(offset, text)=> {
+      //declarando variables
+          let resultadoEncode = '';
+       
+          for (let i=0; i < text.length; i++){
+              let asciiCode = text.charCodeAt(i);
+       
+              if (asciiCode >= 65 && asciiCode <= 90){
+                  asciiCode = (asciiCode - 65 + offset) % 26 + 65;
+              }
+           resultadoEncode +=String.fromCharCode(asciiCode);
+          }
+       
+          return resultadoEncode;
+       },
+      
+  decode :(offset, text) => {
+       
+          let resultadoDecode = '';
+       
+          for (let i = 0; i < text.length; i++) {
+       
+              let asciiCode = text.charCodeAt(i);
+       if (asciiCode >= 65 && asciiCode <= 90) {
+                  asciiCode = ((asciiCode - 90 - offset) % 26 + 90)
+          }
+          resultadoDecode+=String.fromCharCode(asciiCode);
+       }
+          return resultadoDecode;
+       }
+      
+      
+      };
+      
+  
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-/* Acá va tu código */
+const boton = document.getElementById("botonNombre");
+boton.addEventListener("click", () =>{
+let nameChica = document.getElementById("nombreChica").value;
+document.getElementById("aparecedorNombre").innerHTML =`<h2>${nameChica}¿Cúal es tu numero favorito?</h2>`;
+})
+
+const botonCifrado = document.getElementById("botonCifrar");
+//const textDescifrar = document.getElementById("cajaDescifrado").value;
+
+botonCifrado.addEventListener("click",() =>{
+ let textCifrar= document.getElementById("cajaCifrado").value;
+ const offset = document.getElementById("numeroFav").value;
+  document.getElementById("aparecedorCaja1").innerHTML = `${textCifrar}  ${offset}`;
+})
+
+const botonDescifrado = document.getElementById("botonDescifrar");
+botonDescifrado.addEventListener("click",() =>{
+let textDescifrar = document.getElementById("cajaDescifrado").value;
+const offset = document.getElementById("numeroFav").value;
+console.log(textDescifrar + offset);
+})
+
+
+
